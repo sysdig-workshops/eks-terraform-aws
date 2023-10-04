fb2dfa01a71290885ca74cd59964e9931caaef06
vhernandomartin <vhernandomartin@gmail.com>
2023-10-04 16:24:59 +0200

    disabled EBS CSI driver from terraform

--- main.tf
+++ main.tf
@@ -99,13 +99,13 @@ module "irsa-ebs-csi" {
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
 }
 
-resource "aws_eks_addon" "ebs-csi" {
-  cluster_name             = module.eks.cluster_name
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.5.2-eksbuild.1"
-  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-  tags = {
-    "eks_addon" = "ebs-csi"
-    "terraform" = "true"
-  }
-}
+#resource "aws_eks_addon" "ebs-csi" {
+#  cluster_name             = module.eks.cluster_name
+#  addon_name               = "aws-ebs-csi-driver"
+#  addon_version            = "v1.5.2-eksbuild.1"
+#  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+#  tags = {
+#    "eks_addon" = "ebs-csi"
+#    "terraform" = "true"
+#  }
+#}
