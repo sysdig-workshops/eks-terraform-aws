abd20a9950d01860f2e7285ad34a851aba344ae8
manuelbcd <manuelbcd@gmail.com>
2023-10-17 17:56:12 +0200

    Update main.tf EC2 instances (nodes)

--- main.tf
+++ main.tf
@@ -63,20 +63,20 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t3.large"]
+      instance_types = ["t3.xlarge"]
 
       min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      max_size     = 1
+      desired_size = 1
     }
 
     two = {
       name = "node-group-2"
 
-      instance_types = ["t3.large"]
+      instance_types = ["t3.xlarge"]
 
       min_size     = 1
-      max_size     = 2
+      max_size     = 1
       desired_size = 1
     }
   }
